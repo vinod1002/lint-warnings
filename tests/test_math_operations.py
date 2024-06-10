3f8ffcd2eadbcd1dc68b2d12be09e20e197c5f0f
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-10 20:18:04 +0530

    Create test_math_operations.py

---
+++ tests/test_math_operations.py
@@ -0,0 +1,16 @@
+import unittest
+from math_operations import add, subtract
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1,2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1,-1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(2,1), 1)
+        self.assertEqual(subtract(-1, -1), 0)
+        self.assertEqual(subtract(1,1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
