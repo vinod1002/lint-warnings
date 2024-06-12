2a006eb1099e22d632a9b1875ecdd6ca988f0f81
vinod1002 <158429600+vinod1002@users.noreply.github.com>
2024-06-13 00:38:55 +0530

    Create CalculatorTest.java

---
+++ tests/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+}
